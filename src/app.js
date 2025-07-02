50b71a98f976bf689ca6fe2b16eb1137d92c46d5
archita172004 <archichaudhary2004@gmail.com>
2025-07-02 23:02:35 +0530

    added app.use configurations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import express from "express";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, //url we want to allow to make req to server
+    credentials: true, // if you're sending cookies/auth headers
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //for getting form data
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // teels express that url will also have the data encoded ,
+// extended is used to tell object inside object is also readable
+
+app.use(express.static("public")); // stores assets like pdfs favicon etc.
+
+app.use(cookieParser());
 
 export { app };
