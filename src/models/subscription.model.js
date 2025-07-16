aea8face589e96dd9d9678884a4a1925a7540a6c
archita172004 <archichaudhary2004@gmail.com>
2025-07-16 23:37:13 +0530

    added subscription model schema

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, //the one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, //one to whom we r is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
